70eef9c551d5c632c63569f959faba6865c0bbd1
Yong-uk Choe <yonguk.choe@gmail.com>
2020-01-29 16:25:05 +0900

    Update install_basic_rpms.sh

--- install_basic_rpms.sh
+++ install_basic_rpms.sh
@@ -6,3 +6,5 @@ yum install -y epel-release
 yum update
 
 yum install -y rsync fish htop python3 vim-enhanced 
+
+curl https://raw.githubusercontent.com/YUChoe/dotfiles/master/.bash_aliases -o ~/.bash_aliases
